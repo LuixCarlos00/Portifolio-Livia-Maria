cf69fd3a6efbe065fdfa5c233fb9ef83eaefc5f4
LuixCarlos00 <107255451+LuixCarlos00@users.noreply.github.com>
2024-11-06 16:20:34 -0300

    Luiz Carlos 06/11/2024
    - Foi feito a area de contado e estilizado com css

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
+
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
